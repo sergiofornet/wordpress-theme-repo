e48ab5b4c1effab0c5b4f7f9ae0dc5973bd7cdaa
sergiofornet <sergio.fornet@gmail.com>
2021-02-17 19:23:25 +0100

    Add comment disabling functions

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,87 @@ function wordpress_theme_repo_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'wordpress_theme_repo_pingback_header' );
+
+// /**
+//  * Disable support for comments and trackbacks in post types.
+//  */ 
+
+// function britaprinz_disable_comments_post_types_support() {
+// 	$post_types = get_post_types();
+// 	foreach ( $post_types as $post_type ) {
+// 		if( post_type_supports( $post_type, 'comments' ) ) {
+// 			remove_post_type_support( $post_type, 'comments' );
+// 			remove_post_type_support( $post_type, 'trackbacks' );
+// 		}
+// 	}
+// }
+// add_action( 'admin_init', 'britaprinz_disable_comments_post_types_support' );
+
+// /**
+//  * Close comments on the front-end.
+//  */ 
+// function britaprinz_disable_comments_status() {
+// 	return false;
+// }
+// add_filter( 'comments_open', 'britaprinz_disable_comments_status', 20, 2);
+// add_filter( 'pings_open', 'britaprinz_disable_comments_status', 20, 2 );
+
+// /**
+//  * Hide existing comments.
+//  */ 
+// function britaprinz_disable_comments_hide_existing_comments( $comments ) {
+// 	$comments = array();
+// 	return $comments;
+// }
+// add_filter( 'comments_array', 'britaprinz_disable_comments_hide_existing_comments', 10, 2 );
+
+// /**
+//  * Remove comments page in menu.
+//  */
+// function britaprinz_disable_comments_admin_menu() {
+// 	remove_menu_page( 'edit-comments.php' );
+// }
+// add_action( 'admin_menu', 'britaprinz_disable_comments_admin_menu' );
+
+// /**
+//  * Remove comments page in admin bar.
+//  */
+// function britaprinz_disable_admin_bar_comments() {
+//     global $wp_admin_bar;
+//     $wp_admin_bar->remove_menu( 'comments' );
+// }
+// add_action( 'wp_before_admin_bar_render', 'britaprinz_disable_admin_bar_comments' );
+
+// /**
+//  * Redirect any user trying to access comments page.
+//  */
+// function britaprinz_disable_comments_admin_menu_redirect() {
+// 	global $pagenow;
+// 	if ( $pagenow === 'edit-comments.php' ) {
+// 		wp_redirect( admin_url() ); exit;
+// 	}
+// }
+// add_action( 'admin_init', 'britaprinz_disable_comments_admin_menu_redirect' );
+
+// /**
+//  * Remove comments metabox from dashboard.
+//  */
+// function britaprinz_disable_comments_dashboard() {
+// 	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+// }
+// add_action( 'admin_init', 'britaprinz_disable_comments_dashboard' );
+
+// /**
+//  * Remove comments links from admin bar.
+//  */
+// function britaprinz_disable_comments_admin_bar() {
+// 	if ( is_admin_bar_showing() ) {
+// 		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+// 	}
+// }
+// add_action( 'init', 'britaprinz_disable_comments_admin_bar' );
+
+// /**
+//  * Disable html in comments.
+//  */
+// add_filter( 'pre_comment_content', 'esc_html' );
