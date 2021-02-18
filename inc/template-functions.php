5b9e5341d645777d019319ae1ce67b0a5d78de4a
sergiofornet <sergio.fornet@gmail.com>
2021-02-18 17:40:07 +0100

    Uncomment template functions

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -36,88 +36,88 @@ function wordpress_theme_repo_pingback_header() {
 }
 add_action( 'wp_head', 'wordpress_theme_repo_pingback_header' );
 
-// /**
-//  * Disable support for comments and trackbacks in post types.
-//  */ 
-
-// function wordpress_theme_repo_disable_comments_post_types_support() {
-// 	$post_types = get_post_types();
-// 	foreach ( $post_types as $post_type ) {
-// 		if( post_type_supports( $post_type, 'comments' ) ) {
-// 			remove_post_type_support( $post_type, 'comments' );
-// 			remove_post_type_support( $post_type, 'trackbacks' );
-// 		}
-// 	}
-// }
+/**
+ * Disable support for comments and trackbacks in post types.
+ */ 
+function wordpress_theme_repo_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ( $post_types as $post_type ) {
+		if ( post_type_supports( $post_type, 'comments' ) ) {
+			remove_post_type_support( $post_type, 'comments' );
+			remove_post_type_support( $post_type, 'trackbacks' );
+		}
+	}
+}
 // add_action( 'admin_init', 'wordpress_theme_repo_disable_comments_post_types_support' );
 
-// /**
-//  * Close comments on the front-end.
-//  */ 
-// function wordpress_theme_repo_disable_comments_status() {
-// 	return false;
-// }
+/**
+ * Close comments on the front-end.
+ */ 
+function wordpress_theme_repo_disable_comments_status() {
+	return false;
+}
 // add_filter( 'comments_open', 'wordpress_theme_repo_disable_comments_status', 20, 2);
 // add_filter( 'pings_open', 'wordpress_theme_repo_disable_comments_status', 20, 2 );
 
-// /**
-//  * Hide existing comments.
-//  */ 
-// function wordpress_theme_repo_disable_comments_hide_existing_comments( $comments ) {
-// 	$comments = array();
-// 	return $comments;
-// }
+/**
+ * Hide existing comments.
+ */ 
+function wordpress_theme_repo_disable_comments_hide_existing_comments( $comments ) {
+	$comments = array();
+	return $comments;
+}
 // add_filter( 'comments_array', 'wordpress_theme_repo_disable_comments_hide_existing_comments', 10, 2 );
 
-// /**
-//  * Remove comments page in menu.
-//  */
-// function wordpress_theme_repo_disable_comments_admin_menu() {
-// 	remove_menu_page( 'edit-comments.php' );
-// }
+/**
+ * Remove comments page in menu.
+ */
+function wordpress_theme_repo_disable_comments_admin_menu() {
+	remove_menu_page( 'edit-comments.php' );
+}
 // add_action( 'admin_menu', 'wordpress_theme_repo_disable_comments_admin_menu' );
 
-// /**
-//  * Remove comments page in admin bar.
-//  */
-// function wordpress_theme_repo_disable_admin_bar_comments() {
-//     global $wp_admin_bar;
-//     $wp_admin_bar->remove_menu( 'comments' );
-// }
+/**
+ * Remove comments page in admin bar.
+ */
+function wordpress_theme_repo_disable_admin_bar_comments() {
+	global $wp_admin_bar;
+	$wp_admin_bar->remove_menu( 'comments' );
+}
 // add_action( 'wp_before_admin_bar_render', 'wordpress_theme_repo_disable_admin_bar_comments' );
 
-// /**
-//  * Redirect any user trying to access comments page.
-//  */
-// function wordpress_theme_repo_disable_comments_admin_menu_redirect() {
-// 	global $pagenow;
-// 	if ( $pagenow === 'edit-comments.php' ) {
-// 		wp_redirect( admin_url() ); exit;
-// 	}
-// }
+/**
+ * Redirect any user trying to access comments page.
+ */
+function wordpress_theme_repo_disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ( 'edit-comments.php' === $pagenow ) {
+		wp_redirect( admin_url() );
+		exit;
+	}
+}
 // add_action( 'admin_init', 'wordpress_theme_repo_disable_comments_admin_menu_redirect' );
 
-// /**
-//  * Remove comments metabox from dashboard.
-//  */
-// function wordpress_theme_repo_disable_comments_dashboard() {
-// 	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-// }
+/**
+ * Remove comments metabox from dashboard.
+ */
+function wordpress_theme_repo_disable_comments_dashboard() {
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+}
 // add_action( 'admin_init', 'wordpress_theme_repo_disable_comments_dashboard' );
 
-// /**
-//  * Remove comments links from admin bar.
-//  */
-// function wordpress_theme_repo_disable_comments_admin_bar() {
-// 	if ( is_admin_bar_showing() ) {
-// 		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
-// 	}
-// }
+/**
+ * Remove comments links from admin bar.
+ */
+function wordpress_theme_repo_disable_comments_admin_bar() {
+	if ( is_admin_bar_showing() ) {
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+	}
+}
 // add_action( 'init', 'wordpress_theme_repo_disable_comments_admin_bar' );
 
-// /**
-//  * Disable html in comments.
-//  */
+/**
+ * Disable html in comments.
+ */
 // add_filter( 'pre_comment_content', 'esc_html' );
 
 /**
@@ -165,20 +165,20 @@ function wordpress_theme_repo_disable_author_page() {
 	
 	global $wp_query;
 	
-    if ( is_author() ) {
+	if ( is_author() ) {
 		
 		// show a 404 page
-        $wp_query->set_404();
+		$wp_query->set_404();
 		status_header( 404 );
 		
-    }
+	}
 }
-add_action( 'template_redirect', 'wordpress_theme_repo_disable_author_page');
+add_action( 'template_redirect', 'wordpress_theme_repo_disable_author_page' );
 
 /**
  * Modify default queries
  */
-// function wordpress_theme_repo_modify_queries( $query ) {
-	
-// };
-// add_action( 'pre_get_posts', 'wordpress_theme_repo_modify_queries' ); 
+function wordpress_theme_repo_modify_queries( $query ) {
+
+};
+// add_action( 'pre_get_posts', 'wordpress_theme_repo_modify_queries' );
